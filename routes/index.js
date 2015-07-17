ea34e5a83ce7e5ec727068db7095cefb10418eb5
PilarFL <mpilarferrer@gmail.com>
2015-07-18 01:43:07 +0200

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -9,16 +9,14 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+//Autoload de comandos con :quidId
+router.param('quizId', quizController.load);  // autoload :quizId
+
 /*Definición de rutas de /quizes*/
 router.get('/quizes',                       quizController.index);
 router.get('/quizes/:quizId(\\d+)',         quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',  quizController.answer);
 
 router.get('/autor', quizController.autor);
-
-/*nuevas rutas
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
-router.get('/autor', quizController.autor);*/
 
 module.exports = router;
