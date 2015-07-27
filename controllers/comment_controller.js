e13658c12f31ea2c595df4433c162b3087256bb6
PilarFL <mpilarferrer@gmail.com>
2015-07-27 19:06:46 +0200

    Moderacion de comentarios

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,5 +1,17 @@
 var models = require('../models/models.js');
 
+//Autoload :id de comentarios
+exports.load = function(req, res, next, commentId){
+  models.Comment.find({
+        where: { id: Number (commentId)}
+  }).then(function(comment){
+      if(comment){
+        req.comment = comment;
+        next();
+      }else{ next(new Error('No existe el comentario ' + commentId))}
+  }).catch(function(error){next(error)});
+};
+
 //Controladores de comentarios asociados a las acciones create y new
 //new : asociado a la formulario de visualización de un nuevo comentario.
 //create: asociado a la formulario de creación de un nuevo comentario.
@@ -24,4 +36,13 @@ exports.create = function(req,res) {
       console.log('GUARDAMOS EN LA DB  :   '+comment);
       comment.save().then( function(){res.redirect('/quizes/'+req.params.quizId)})
     } //res.redirect: Redirección HTTP a la lista de preguntas
+  };
+
+  //GET /quizes/:quizId/comments/:commentId/publish
+  exports.publish = function(req,res){
+    req.comment.publicado = true;
+
+    req.comment.save({fields: ["publicado"]})
+       .then(function(){res.redirect('/quizes/' + req.params.quizId);} )
+       .catch(function(error){next(error)});
   };
--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ router.get('/', function(req, res) {
 
 //Autoload de comandos con :quidId
 router.param('quizId', quizController.load);  // autoload :quizId
+router.param('commentId', commentController.load); //autoload :commentId
 
 //Definicion de rutas de sesion
 router.get('/login',sessionController.new);       // formulario login
@@ -30,7 +31,10 @@ router.get('/quizes/:quizId(\\d+)/edit',    sessionController.loginRequired,  qu
 router.put('/quizes/:quizId(\\d+)',         sessionController.loginRequired,  quizController.update);
 router.delete('/quizes/:quizId(\\d+)',      sessionController.loginRequired,  quizController.destroy);
 
+//Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new',   commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',   commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+              sessionController.loginRequired, commentController.publish);
 
 module.exports = router;
